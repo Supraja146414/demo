70028212e6a4b97df67e5d625304a37bce0acb9c
shaik afreen <afreen25.shaik@gmail.com>
2025-03-09 14:21:01 +0000

    changes

--- macros/deptsalary.sql
+++ macros/deptsalary.sql
@@ -1,7 +1,5 @@
 {%macro deptsalary() %}
 
-
-
 SELECT d.department_name, SUM(e.salary) AS total_salary
     FROM prod.departments d 
     INNER JOIN prod.employees e ON e.department_id = d.department_id
