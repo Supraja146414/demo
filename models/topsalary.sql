173eee9697df77b7dbb9ae0c4156672a60682df8
shaik afreen <afreen25.shaik@gmail.com>
2025-03-09 14:24:00 +0000

    changes

--- models/topsalary.sql
+++ models/topsalary.sql
@@ -1,7 +1,6 @@
-SELECT A.department_name, A.total_salary,DENSE_RANK() OVER(ORDER BY total_salary DESC) AS rank_
-FROM (
-
-    {{ deptsalary() }}
-
+SELECT A.department_name, A.total_salary, 
+       DENSE_RANK() OVER(ORDER BY total_salary DESC) AS rank_
+FROM ( 
+    SELECT * FROM {{ ref('deptsalary') }} 
 ) AS A
 QUALIFY rank_ < 3;
