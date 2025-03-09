bd35edca124b7d7f23791eaf02e3a36c71bdd3db
shaik afreen <afreen25.shaik@gmail.com>
2025-03-09 14:24:42 +0000

    changes

--- macros/deptsalary.sql
+++ macros/deptsalary.sql
@@ -1,8 +1,7 @@
-{%macro deptsalary() %}
+{% macro deptsalary() %}
+SELECT department_name, SUM(salary) AS total_salary
+FROM prod.departments d
+INNER JOIN prod.employees e ON e.department_id = d.department_id
+GROUP BY department_name
+{% endmacro %}
 
-SELECT d.department_name, SUM(e.salary) AS total_salary
-    FROM prod.departments d 
-    INNER JOIN prod.employees e ON e.department_id = d.department_id
-    GROUP BY 1
-
-    {% endmacro %}
